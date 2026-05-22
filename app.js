801a68ead8cfab894db4edacaa5e0b5ec8cca446
ArielYM6 <yeraariel0@gmail.com>
2026-05-22 17:03:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,36 @@ let currentUser = null;
 let config = null;
 let allAppointments = [];
 let isAdmin = false;
+async function sendTelegramNotification(text){
 
+  const BOT_TOKEN =
+  "8637382666:AAFCS4IxrPtIsgRLEKdY39z3fBusDlgSqbs";
+
+  const CHAT_ID =
+  "999753868";
+
+  const url =
+  `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+  await fetch(url,{
+
+    method:"POST",
+
+    headers:{
+      "Content-Type":"application/json"
+    },
+
+    body:JSON.stringify({
+
+      chat_id:CHAT_ID,
+
+      text:text
+
+    })
+
+  });
+
+}
 checkSession();
 
 // SESIÓN
